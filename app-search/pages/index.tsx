32dbcb952f0ef7bf4e1c5c33005a2949ecdef5d9
Ivan Kleshnin <ivan@paqmind.com>
2023-08-27 19:22:48 +0300

    Improve UX

--- app-search/pages/index.tsx
+++ app-search/pages/index.tsx
@@ -1,7 +1,16 @@
 import {Container} from "@/common/components"
 import {Label, TextInput} from "@/common/components/Form"
+import {
+  ChangeEventHandler,
+  forwardRef,
+  memo,
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState
+} from "react"
 import {twMerge} from "tailwind-merge"
-import {forwardRef, useCallback, useEffect, useRef, useState} from "react"
 import styles from "./slider.module.css"
 
 export default function SliderPage() {
@@ -12,17 +21,18 @@ export default function SliderPage() {
 
   const [from, setFrom] = useState(defaultFrom)
   const [to, setTo] = useState(defaultTo)
+  const [dragging, setDragging] = useState<"from" | "to" | undefined>(undefined)
 
   const fromInputRef = useRef<HTMLInputElement>(null!)
   const toInputRef = useRef<HTMLInputElement>(null!)
   const trackRef = useRef<any>(null!)
 
   useEffect(function fillColor() {
-    const fromValue = parseInt(fromInputRef.current.value)
-    const toValue = parseInt(toInputRef.current.value)
+    const from = parseInt(fromInputRef.current.value)
+    const to = parseInt(toInputRef.current.value)
 
-    const fromPercent = fromValue * 100 / max
-    const toPercent = toValue * 100 / max
+    const fromPercent = from * 100 / max
+    const toPercent = to * 100 / max
     trackRef.current.style.background = `linear-gradient(
       to right, 
       #cbd5e1 ${fromPercent}%, 
@@ -33,35 +43,47 @@ export default function SliderPage() {
   })
 
   const onFromChange = useCallback(() => {
-    const fromValue = parseInt(fromInputRef.current.value)
-    const toValue = parseInt(toInputRef.current.value)
+    const from = parseInt(fromInputRef.current.value)
+    const to = parseInt(toInputRef.current.value)
 
-    if (fromValue <= toValue) {
+    if (from <= to) {
       // Normal case: just update the displayed value
-      setFrom(fromValue)
+      setFrom(from)
     } else {
       // Crossover case: flip input and displayed values
-      fromInputRef.current.value = String(toValue)
-      toInputRef.current.value = String(fromValue)
-      setFrom(toValue)
-      setTo(fromValue)
+      fromInputRef.current.value = String(to)
+      toInputRef.current.value = String(from)
+      setFrom(to)
+      setTo(from)
     }
   }, [])
 
   const onToChange = useCallback(() => {
-    const fromValue = parseInt(fromInputRef.current.value)
-    const toValue = parseInt(toInputRef.current.value)
+    const from = parseInt(fromInputRef.current.value)
+    const to = parseInt(toInputRef.current.value)
 
-    if (toValue >= fromValue) {
+    if (to >= from) {
       // Normal case: just update the displayed value
-      setTo(toValue)
+      setTo(to)
     } else {
       // Crossover case: flip input and displayed values
-      toInputRef.current.value = String(fromValue)
-      fromInputRef.current.value = String(toValue)
-      setTo(fromValue)
-      setFrom(toValue)
+      toInputRef.current.value = String(from)
+      fromInputRef.current.value = String(to)
+      setTo(from)
+      setFrom(to)
     }
+  }, [])
+
+  const onFromMouseDown = useCallback(() => {
+    setDragging("from")
+  }, [])
+
+  const onToMouseDown = useCallback(() => {
+    setDragging("to")
+  }, [])
+
+  const onMouseUp = useCallback(() => {
+    setDragging(undefined)
   }, [])
 
   return <>
@@ -74,25 +96,27 @@ export default function SliderPage() {
       </div>
       <div className={styles.slider}>
         <div className="flex justify-between mt-1">
-          <span className="px-2 select-none rounded-sm leading-6 bg-white text-sm font-medium text-purple-500">
-            {from}
-          </span>
-          <span className="px-2 select-none rounded-sm leading-6 bg-white text-sm font-medium text-purple-500">
-            {to >= max ? <>&infin;</> : to}
-          </span>
+          <ValueBox value={from}/>
+          <ValueBox value={to >= max ? Infinity : to}/>
         </div>
         <div className="relative mt-4">
           <Track ref={trackRef}/>
           <RangeInput
+            className={dragging == "from" || from == max ? "z-10" : ""}
             defaultValue={defaultFrom}
             min={min} max={max}
             onChange={onFromChange}
+            onMouseDown={onFromMouseDown}
+            onMouseUp={onMouseUp}
             ref={fromInputRef}
           />
           <RangeInput
+            className={dragging == "to" ? "z-10" : ""}
             defaultValue={defaultTo}
             min={min} max={max}
             onChange={onToChange}
+            onMouseDown={onToMouseDown}
+            onMouseUp={onMouseUp}
             ref={toInputRef}
           />
         </div>
@@ -101,10 +125,32 @@ export default function SliderPage() {
   </>
 }
 
-const RangeInput = forwardRef(function RangeInput({className, ...rest}: any, ref: any) {
-  // original had: margin: auto
-  // original had: outline-none
-  // className="bg-transparent"
+// ValueBox
+type ValueBoxProps = {
+  value: number
+}
+
+const ValueBox = memo(function ValueBox(props: ValueBoxProps) {
+  const {value} = props
+  return <span className="px-2 select-none rounded-sm leading-6 bg-white text-sm font-medium text-purple-500">
+    {isFinite(value) ? value : <>&infin;</>}
+  </span>
+})
+
+// RangeInput
+type RangeInputProps = {
+  className?: string
+  defaultValue?: number
+  min?: number
+  max?: number
+  onChange?: ChangeEventHandler<HTMLInputElement>
+  onMouseDown?: MouseEventHandler<HTMLInputElement>
+  onMouseUp?: MouseEventHandler<HTMLInputElement>
+  value?: number
+}
+
+const RangeInput = forwardRef<HTMLInputElement, RangeInputProps>(function RangeInput(props, ref) {
+  const {className, ...rest} = props
   return <input
     className={twMerge(
       "absolute inset-0 w-full",
@@ -117,40 +163,20 @@ const RangeInput = forwardRef(function RangeInput({className, ...rest}: any, ref
   />
 })
 
-const Track = forwardRef(function Track(_: any, ref: any) {
+// Track
+type TrackProps = {
+  className?: string
+}
+
+const Track = memo(forwardRef<HTMLDivElement, TrackProps>(function Track(props, ref) {
+  const {className, ...rest} = props
   return <div
     className={twMerge(
       "absolute inset-0 my-auto",
-      "w-full inset-0 rounded-full h-1 slider-track bg-white"
+      "w-full inset-0 rounded-full h-1 slider-track bg-white",
+      className
     )}
     ref={ref}
+    {...rest}
   />
-})
-
-// const slider1 = document.getElementById('slider-1') as HTMLInputElement
-// const slider2 = document.getElementById('slider-2') as HTMLInputElement
-// const rangeDisplay1 = document.getElementById('range1')!
-// const rangeDisplay2 = document.getElementById('range2')!
-// const sliderTrack = document.querySelector('.slider-track') as HTMLDivElement
-//
-// function slideOne() {
-//   if (parseInt(slider2.value) < parseInt(slider1.value)) {
-//     slider1.value = slider2.value
-//   }
-//   rangeDisplay1.textContent = slider1.value
-//   fillColor()
-// }
-//
-// function slideTwo() {
-//   if (parseInt(slider2.value) < parseInt(slider1.value)) {
-//     slider2.value = slider1.value
-//   }
-//   rangeDisplay2.textContent = slider2.value
-//   fillColor()
-// }
-//
-// function fillColor() {
-//   const percent1 = (parseInt(slider1.value) / parseInt(slider1.max)) * 100
-//   const percent2 = (parseInt(slider2.value) / parseInt(slider1.max)) * 100
-//   sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , rgb(79 70 229) ${percent1}% , rgb(79 70 229) ${percent2}% , #dadae5 ${percent2}%)`
-// }
+}))
